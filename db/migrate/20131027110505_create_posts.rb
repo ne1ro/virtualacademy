d75cd1a221714f94280ea73ceac8991662f2a385
neiro <neiro.mail@gmail.com>
2013-10-27 13:06:52 +0200

    Generate post model

---
+++ db/migrate/20131027110505_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title, null: false
+      t.text :body, null: false
+      t.boolean :published, default: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131027105954) do
+ActiveRecord::Schema.define(version: 20131027110505) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title",                      null: false
+    t.text     "body",                       null: false
+    t.boolean  "published",  default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tasks", force: true do |t|
     t.string  "title",     null: false
