b85a9eb4b16f79930bc5972bf775a3186956ff51
neiro <neiro.mail@gmail.com>
2013-12-09 22:43:36 +0400

    Use slim and sass instead of html and css

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131027112121) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "courses", force: true do |t|
     t.string   "title",                      null: false
     t.string   "description",                null: false
