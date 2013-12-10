a9c13cf39a8693124387a50f906528a6f1b57680
neiro <neiro.mail@gmail.com>
2013-12-10 21:47:57 +0400

    Change devise models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
-end
+  devise :database_authenticatable, :registerable, :recoverable, :trackable, :validatable, :omniauthable, :confirmable
+  end
