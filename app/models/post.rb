a6fb15fa71dd207b13dd58f16edd09cfa68e72d6
neiro <neiro.mail@gmail.com>
2013-12-10 20:44:25 +0400

    Create post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ActiveRecord::Base
-  
+  validates :title, :body, :published, presence: true
+  validates :title, uniqueness: true
+  validates :title, length: {minimum: 3, maximum: 120}
+  validates :title, length: {minimum: 3, maximum: 120}
+
+  # Convert to slug url
+  def to_param
+    title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
 end
