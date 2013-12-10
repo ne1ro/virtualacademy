028a7bfe88a22bd9aefc6a947835a3acc4fd8c09
neiro <neiro.mail@gmail.com>
2013-12-10 20:35:49 +0400

    Add some test to post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,8 +5,18 @@ describe Post do
     FactoryGirl.create(:post).should be_valid
   end
 
-  it 'is invalid without title' do
-    FactoryGirl.build(:post, title: nil).should_not be_valid
+  context '#title' do
+    it 'is invalid without' do
+      FactoryGirl.build(:post, title: nil).should_not be_valid
+    end
+  
+    it 'should have valid slug ' do
+      FactoryGirl.build(:post, title: 'My first blog #p0st').should be 'my-first-blog-post'
+    end
+
+    it 'should have fixed length' do
+      FactoryGirl.build(:post, title: '11029i 0192wi0192iw 091i2w09i12i9010892f 098f 019 0912i09 1029 0129f0912 0f912u 0912i 0921093').should_not be_valid
+    end
   end
 
   it 'is invalid without body' do
