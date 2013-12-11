60c0ac2fee1741f28521b581a88771db30c15294
neiro <neiro.mail@gmail.com>
2013-12-11 09:56:38 +0400

    Create user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,11 @@ require 'faker'
 
 FactoryGirl.define do
   factory :user do
+    fullname { Faker::Name.first_name + Faker::Name.last_name }
+    photo { Faker::Internet.url }
+    email { Faker::Internet.email }
+    about { Faker::Lorem.sentence + Faker::Lorem.sentence }
+    nickname { Faker::Internet.slug }
+    location { Faker::Address.state }
   end
 end
