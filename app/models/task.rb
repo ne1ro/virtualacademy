6c270a4897d38f5eaf791b559eed4e5782358683
neiro <neiro.mail@gmail.com>
2013-12-12 16:25:14 +0400

    Create task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to :lesson
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :number, numericality: {only_integer: true, greater_than: 0}
 end
