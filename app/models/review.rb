c4d1255fa84bd382b82dc6bff65645fe6cd141ef
neiro <neiro.mail@gmail.com>
2013-12-12 16:38:55 +0400

    Create review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :course
+  validates :text, presence: true
+  validates :rate, numericality: {only_integer: true, greater_than: 0, less_than_or_equal_to: 10}
 end
