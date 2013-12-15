1508af70dec46d771882e0887c575ea22d106ce3
neiro <neiro.mail@gmail.com>
2013-12-15 10:31:41 +0400

    Create lesson model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,12 @@
 class Lesson < ActiveRecord::Base
   belongs_to :course
+  validates :title, presence: true, uniqueness: true, length: {maximum: 20, tokenizer: lambda { |str| str.scan(/\w+|[а-яА-Я]+/)}, too_long: "must have at most %{count} words"}
+  validates :description, presence: true
+  validates :body, presence: true
+  validates :number, numericality: {less_than_or_equal_to: 1, only_integer: true}
+
+  # Convert to slug url
+  def to_param
+    title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
 end
