affba6d648456b8b14683661d36b1b79781ee7bc
neiro <neiro.mail@gmail.com>
2013-12-24 12:21:46 +0400

    Create review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,23 @@
 class ReviewsController < ApplicationController
   def index
+    @reviews = Review.all
+  end
+
+  def show
+    @review = Review.find params[:id]
   end
 
   def create
-  end
-
-  def update
+    @review = Review.create review_params
   end
 
   def destroy
+    @review = Review.find params[:id]
+    @review.destroy
+  end
+
+private
+  def review_params
+    params.require(:review).permit :text, :rate
   end
 end
