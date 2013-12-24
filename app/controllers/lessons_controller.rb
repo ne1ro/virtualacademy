4099b1c38911df92dc4f98b1b09d2d455ad9797d
neiro <neiro.mail@gmail.com>
2013-12-24 22:19:09 +0400

    Fill lesson controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,16 +1,28 @@
 class LessonsController < ApplicationController
   def index
+    @lessons = Lesson.all
   end
 
   def show
+    @lesson = Lesson.find params[:id]
   end
 
   def create
+    @lesson = Lesson.create lesson_params
   end
 
   def update
+    @lesson = Lesson.find params[:id]
+    @lesson.update_attributes lesson_params
   end
 
   def destroy
+    @lesson = Lesson.find params[:id]
+    @lesson.destroy
+  end
+
+private
+  def lesson_params
+    params.require(:lesson).permit :title, :body, :number, :description
   end
 end
