a559ac57239a3de9c08546d0ce494242504b5881
neiro <neiro.mail@gmail.com>
2013-12-24 22:32:15 +0400

    Fill course ctrl

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,16 +1,28 @@
 class CoursesController < ApplicationController
   def index
+    @courses = Course.all
   end
 
   def show
+    @course = Course.find params[:id]
   end
 
   def create
+    @course = Course.create course_params
   end
 
   def update
+    @course = Course.find params[:id]
+    @course.update_attributes course_params
   end
 
   def destroy
+    @course = Course.find params[:id]
+    @course.destroy
+  end
+
+private
+  def course_params
+    params.require(:course).permit :title, :body, :description, :cost
   end
 end
